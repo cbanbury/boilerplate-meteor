0406c838c6ba0749b3195738fe596c0c76408f90
Carl Banbury <carl@googolmail.co.uk>
2015-08-27 14:21:06 +0100

    adding seo support

--- client/home/home.js
+++ client/home/home.js
@@ -1,3 +1,16 @@
-Template.home.rendered = function() {
+Template.home.onRendered(function() {
     $('.modal-trigger').leanModal();
-}
+});
+
+Template.home.onCreated(function() {
+    var title = 'Meteor boilerplate';
+    var description = 'Heroku Compatible Meteor Boilerplate with FlowRouter, Materialize and other default goodies';
+    SEO.set({
+        title: title,
+        description: description,
+        meta: {
+            'property="og:title"': title,
+            'property="og:description"': description
+        }
+    });
+});
