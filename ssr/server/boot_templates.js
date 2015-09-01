fc6b5acf569359e5e54f479432f1e8dafa34c35f
Carl Banbury <carl@googolmail.co.uk>
2015-09-01 10:48:41 +0100

    Revert "not using spiderable, using server side rendering"
    This reverts commit 59b25e8852c628c7e0fe816e3243f54cd350abba.

--- ssr/server/boot_templates.js
+++
@@ -1,8 +0,0 @@
-SSR.compileTemplate('masterLayout', Assets.getText('master.html'));
-Template.masterLayout.helpers({
-  getDocType: function() {
-    return "<!DOCTYPE html>";
-  }
-});
-
-SSR.compileTemplate('home', Assets.getText('home.html'));
