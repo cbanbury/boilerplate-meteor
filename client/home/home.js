020ad74f0ad73aee4100301b848a2c6bc3db337c
Carl Banbury <carl@googolmail.co.uk>
2015-09-05 16:51:50 +0100

    removing seo stuff

--- client/home/home.js
+++ client/home/home.js
@@ -1,16 +1,3 @@
 Template.home.onRendered(function() {
     $('.modal-trigger').leanModal();
 });
-
-Template.home.onCreated(function() {
-    var title = 'Meteor boilerplate';
-    var description = 'Heroku Compatible Meteor Boilerplate with FlowRouter, Materialize and other default goodies';
-    SEO.set({
-        title: title,
-        description: description,
-        meta: {
-            'property="og:title"': title,
-            'property="og:description"': description
-        }
-    });
-});
